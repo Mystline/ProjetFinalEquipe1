2f857708ff9f80d0363cb91897f33a579010fd8b
Mystline <1142674@LLBD0604INFO15.laboratoire.collegeem.qc.ca>
2015-12-01 10:03:13 -0500

    transport part 1

--- ProjetFinalTeam1Angular/transports/transports.js
+++ ProjetFinalTeam1Angular/transports/transports.js
@@ -4,5 +4,29 @@ angular
  
 function TransportController($scope, $rootScope, $http, $route, $sce) 
 {            
-   
+   if (navigator.geolocation) {
+       
+            navigator.geolocation.watchPosition(function(position) {
+                
+            $scope.latitude = position.coords.latitude;
+            $scope.longitude = position.coords.longitude;  
+                
+            $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+position.coords.latitude+','+position.coords.longitude+'&key='+API_KEY)
+            .success(function(data){
+                $scope.belleAdresse = data.results[0].formatted_address;
+                loc = data.results[0].geometry.location;
+                neb = data.results[0].geometry.viewport.northeast;
+                swb = data.results[0].geometry.viewport.southwest;                      
+                $scope.location = { latitude: loc.lat, longitude: loc.lng }
+                $scope.center = $scope.location;
+                $scope.bounds = { northeast : { latitude: neb.lat, longitude: neb.lng }, southwest : { latitude: swb.lat, longitude: swb.lng } };
+            });
+                
+        });
+       
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+    
+    $scope.center = { latitude: 45.501459, longitude: -73.567543 };
 }]);
