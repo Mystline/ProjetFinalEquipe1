89538c341b625f9d720048392b5a0f1e67e20f79
Mystline <1142674@LLBD0604INFO15.laboratoire.collegeem.qc.ca>
2015-12-08 11:08:51 -0500

    Journee js html

--- ProjetFinalTeam1Angular/jours/jours.js
+++ ProjetFinalTeam1Angular/jours/jours.js
@@ -4,6 +4,62 @@ angular
  
 function JourController($scope, $rootScope, $http, $route, $sce) 
 {
-   
+    
+    $rootScope.JourSelect = "";
+    
+    //rootScope ??
+    $scope.lstJours = [];
+    $scope.lstActivites = [];
+    
+    
+    $scope.getJours(function() {
+    $.ajax({
+        method: 'GET',
+        url: "http://localhost:3216/api/Jour/GetJours",
+        data:
+        {
+            Jour: $rootScope.JourSelect,
+        },
+        error: function (data) {
+            console.log("Aucun jour de selectionner ou autre erreur")
+        }
+    }).success(response)
+    {
+        console.log(response);                            
+      
+        for(var i =0; i< response.length; i++)
+        {
 
+        }
+                                  
+        console.log($scope.lstJours);
+                                  
+    }
+
+    });
+      
+    
+    $scope.getActivities(function() {
+    $.ajax({
+        method: 'GET',
+        url: "http://localhost:3216/api/Activite/GetActivites",
+        data:
+        {
+            Jour: $rootScope.VoyageSelect,
+        },
+        error: function (data) {
+            console.log("Aucune activite de selectionner ou autre erreur")
+        }
+    }).success(response)
+    {
+        console.log(response);
+        for(var i =0; i< response.length; i++)
+        {
+
+        }
+        console.log($scope.lstActivites);
+    }
+
+    });
+                                  
 }]);
