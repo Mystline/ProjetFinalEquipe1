d85ac0673788c611da6447db08c8733a5aa50e08
Darktechnocracius <1041785@LLBD0604INFO01.laboratoire.collegeem.qc.ca>
2015-12-15 14:01:56 -0500

    activity modif

--- ProjetFinalTeam1Angular/activites/activites.js
+++ ProjetFinalTeam1Angular/activites/activites.js
@@ -6,39 +6,38 @@ function ActiviteController($scope, $rootScope, $http, $route, $sce)
 {            
     
     
-           scope.HeureDebut = "";
+           $scope.HeureDebut = "";
             $scope.HeureFin = "";
             $scope.Cout = "";
+        $scope.Cout = "";
+    $scope.Latitude = "";
+    $scope.Longitude = "";
+
             $scope.activites = [];
     
     
- /*   $scope.ajouterActivites = function () {
+    $scope.ajouterActivites = function () {
                 
          
                 
-                if ($scope.HeureDebut == "" || $scope.HeureFin == "" || $scope.Cout == "") {
+                if($scope.HeureDebut == "" || $scope.HeureFin == "" || $scope.Cout == ""||$scope.Latitude == ""||$scope.Longitude == ""){
 
 
                     alert("Un ou plusieurs champs ne sont pas valides");
 
-                } else {
-                    
-                    var dt = new Date();
-                    var dtstring = dt.getFullYear()
-                    + '-' + pad2(dt.getMonth()+1)
-                    + '-' + pad2(dt.getDate())
-                    + ' ' + pad2(dt.getHours())
-                    + ':' + pad2(dt.getMinutes())
-                    + ':' + pad2(dt.getSeconds());
-                    
-                    console.log($scope.dateDebut,$scope.budgetVoyage,$scope.nbJours);
+                }else {
+                            
                     $.ajax({
                         method: 'POST',
                         url: "http://localhost:3216/api/Activites/",
                         data: {
                             HeureDebut: $scope.HeureDebut,
                             HeureFin: $scope.HeureFin,
-                            Cout: $scope.Cout
+                            Cout: $scope.Cout,
+                            Latitude:$scope.Latitude,
+                            Longitude:$scope.Longitude,
+                            Jour:$rootScope.JourSelect
+                            
                         }
 
                     }).success(function (data)  {
@@ -53,8 +52,6 @@ function ActiviteController($scope, $rootScope, $http, $route, $sce)
 
             }
     
-    function pad2(number) {
-                return (number < 10 ? '0' : '') + number
-            } */
+    
     
 }]);
