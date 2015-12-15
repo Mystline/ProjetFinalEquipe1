87bc6fc3699768b7112959195ff256d7a3f87bf9
Derek Evans <derek1007@live.ca>
2015-12-15 16:51:51 -0500

    CompleteConversionOfLoginJsToAngular

--- ProjetFinalTeam1Angular/Login/Login.js
+++ ProjetFinalTeam1Angular/Login/Login.js
@@ -76,88 +76,73 @@ function LoginController($rootScope, $scope, $http, $location,$sce) {
     }
 
 
-    function init() {
-        $('#login-pass-r').on('change keypress paste focus textInput input', function () {
-            changePassword.apply(this);
+    $scope.myFunct = function (keyEvent) {
+        $timeout(function () {
+            changePassword();
         });
-        $('#login-pass').on('change keypress paste focus textInput input', function () {
-            changePassword.apply(this);
-        });
+       
     }
-
     function changePassword() {
-        var id = $(this).attr('id');
-        var i1, i2;
+
         var mess = "";
         errors = 0;
 
-        if (id === "login-pass") {
-            id1 = "#login-pass";
-            id2 = "#login-pass-r";
+       
+        if ($scope.suPass.length < 6) {
+            $scope.StylePass = { 'border': '1px solid #e74c3c' };
+            $scope.StylePassC = { 'border': '1px solid #e74c3c' };
+            mess += "<li class='red'>La longueur d'un mot de passe doit être d'au moins six caractéres.</li>";
+            errors++;
         } else {
-            id2 = "#login-pass";
-            id1 = "#login-pass-r";
+
         }
 
-        if ($scope.suPass === $scope.suPassC && $scope.suPass.length > 5) {
-          //  $scope.suPass.css = "border", "1px solid #1abc9c";
-         //   $scope.suPassC.css = "border", "1px solid #1abc9c";
-          //$(id2).css("border", "1px solid #1abc9c");
-         //$(id1).css("border", "1px solid #1abc9c");
-            mess += "<li class='green'>Les mots de passe corespondent. </li>";
+        if (!/[A-Z]/.test($scope.suPass)) {
+            $scope.StylePass = { 'border': '1px solid #e74c3c' };
+            $scope.StylePassC = { 'border': '1px solid #e74c3c' };
+            mess += "<li class='red'>Le mot de passe doit contenir au moins une lettre majuscule.</li>";
+            errors++;
+        } else {
+
         }
+
+        if (!/[0-9]/.test($scope.suPass)) {
+            $scope.StylePass = { 'border': '1px solid #e74c3c' };
+            $scope.StylePassC = { 'border': '1px solid #e74c3c' };
+            mess += "<li class='red'>Le mot de passe doit contenir au moins un chiffre.</li>";
+            errors++;
+        } else {
+
+        }
+
+        if (!/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/.test($scope.suPass)) {
+            $scope.StylePass = { 'border': '1px solid #e74c3c' };
+            $scope.StylePassC = { 'border': '1px solid #e74c3c' };
+            mess += "<li class='red'>Le mot de passe doit contenir au moins un caractére spécial.</li>";
+            errors++;
+        } else {
+
+        }
+
+
+        if ($scope.suPass === $scope.suPassC ) {
+       }
         else {
-            
-            $(id1).css("border", "1px solid #e74c3c");
-            $(id2).css("border", "1px solid #e74c3c");
+
+            $scope.StylePass = { 'border': '1px solid #e74c3c' };
+            $scope.StylePassC = { 'border': '1px solid #e74c3c' };
             mess += "<li class='red'>Les mots de passe ne corespondent pas. </li>";
             errors++;
         }
 
-        if ($scope.suPass.length < 6) {
-            mess += "<li class='red'>La longueur d'un mot de passe doit être d'au moins six caractéres.</li>";
-            errors++;
-        } else {
-            mess += "<li class='green'>La longueur du mot de passe est d'au moins six caractéres.</li>";
-        }
-
-        if (!/[A-Z]/.test($scope.suPass)) {
-            mess += "<li class='red'>Le mot de passe doit contenir au moins une lettre majuscule.</li>";
-            errors++;
-        } else {
-            mess += "<li class='green'>Le mot de passe contient au moins une lettre majuscule.</li>";
-        }
-
-        if (!/[0-9]/.test($scope.suPass)) {
-            mess += "<li class='red'>Le mot de passe doit contenir au moins un chiffre.</li>";
-            errors++;
-        } else {
-            mess += "<li class='green'>Le mot de passe contient au moins un chiffre.</li>";
-        }
-
-        if (!/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/.test($scope.suPass)) {
-            mess += "<li class='red'>Le mot de passe doit contenir au moins un caractére spécial.</li>";
-            errors++;
-        } else {
-            mess += "<li class='green'>Le mot de passe contient au moins un caractére spécial.</li>";
-        }
-
-        //$('#righterror').html(mess);
-       // $scope.righterror = mess;
-        //$scope.html = mess;
-        
-      
-
         $scope.righterrorhtml = mess;
+      
         $scope.renderHtml($scope.righterrorhtml);
-         $scope.$apply();
+       
     }
 
-     $scope.renderHtml = function (htmlCode) {
-            return $sce.trustAsHtml(htmlCode);
-        };
-
-  
-
-    init();
+    $scope.renderHtml = function (htmlCode) {
+        return $sce.trustAsHtml(htmlCode);
+       
+    };
 }]);
