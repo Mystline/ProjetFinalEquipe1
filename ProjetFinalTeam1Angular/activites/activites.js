d6a77f578127761269d8910e6b6ba74d72ed3537
hotmax9201 <1251753@LLBD0604INFO13.laboratoire.collegeem.qc.ca>
2015-12-15 14:38:14 -0500

    activite

--- ProjetFinalTeam1Angular/activites/activites.js
+++ ProjetFinalTeam1Angular/activites/activites.js
@@ -1,5 +1,5 @@
 angular
-    .module('projetequipe1.activites', [])
+    .module('projetequipe1', [])
     .controller('ActiviteController',['$scope', '$rootScope', '$http', '$route', '$sce',
  
 function ActiviteController($scope, $rootScope, $http, $route, $sce) {
@@ -12,44 +12,6 @@ function ActiviteController($scope, $rootScope, $http, $route, $sce) {
     
     $scope.activites = [];
     
-    //---------------------------------------------------------------------    
-    //GESTION GOOGLE MAP
-    
-    //Variables
-    var map;
-    var center = new google.maps.LatLng(45.501459, -73.567543);
-    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-    
-    //Initialiser la map
-    function initialize() {
-        var mapCanvas = document.getElementById('map');
-        var mapOptions = {
-            center: center,
-            zoom: 8,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-        map = new google.maps.Map(mapCanvas, mapOptions);
-    }
-    initialize();
-    
-    //??????????????
-    google.maps.event.addDomListener(window, 'load', initialize);
-    
-    
-    //---------------------------------------------------------------------
-    //******************************METHODES*******************************
-    
-    //---------------------------------------------------------------------
-    //Markers
-    $scope.afficherMarker = function (lat, long){
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(lat, long),
-            map: map,
-            icon: '../Images/markerBlack.png'
-            //icon: iconBase + 'schools_maps.png'
-            //title: 'Voila un icon noir!'
-        });
-    }
     
     $scope.ajouterActivites = function () {
         if($scope.HeureDebut == "" || $scope.HeureFin == "" || $scope.Cout == ""||$scope.Latitude == ""||$scope.Longitude == "")
