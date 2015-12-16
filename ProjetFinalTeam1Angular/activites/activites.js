4ef585c8775c298e011b27910e6a2c3dd849b24a
Mystline <hugo.deschenes@hotmail.com>
2015-12-15 22:47:18 -0500

    Affichage suggestion activites

--- ProjetFinalTeam1Angular/activites/activites.js
+++ ProjetFinalTeam1Angular/activites/activites.js
@@ -1,8 +1,9 @@
 angular.module('projetequipe1.activites', [])
 
-.controller('ActiviteController',
+.controller('ActiviteController', ActiviteController)
+
             
-function ActiviteController($scope, $rootScope, $http, $route, $sce) {
+function ActiviteController($scope, $rootScope, $http, $route, $sce, $compile) {
     $scope.HeureDebut = "";
     $scope.HeureFin = "";
     $scope.Cout = "";
@@ -40,4 +41,54 @@ function ActiviteController($scope, $rootScope, $http, $route, $sce) {
         }
     }
     
-});
+    
+    var mapOptions = {
+                    center: { lat: 45.501459, lng: -73.567543 },
+                    zoom: 8
+                };
+    var map = new google.maps.Map(document.getElementById('map-places'),mapOptions);
+    var request = {
+        location: { lat: 45.501459, lng: -73.567543 },
+        radius: '10000'
+
+        // https://developers.google.com/places/documentation/supported_types
+        //types: ['food']
+    };
+
+    console.log(map);
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, function (results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            console.log(results);
+            $scope.places = [];
+            $scope.markers = [];
+            $scope.infoWindow = new google.maps.InfoWindow();
+            var bounds = new google.maps.LatLngBounds();
+            for(i=0; i<5; i++) {
+                bounds.extend(results[i].geometry.location);
+                $scope.places.push(results[i]);
+                var marker = new google.maps.Marker({
+                    position: $scope.places[i].geometry.location,
+                    map: map,
+                    icon: results[i].icon
+                })
+                $scope.markers.push(marker);
+                var content = '<div><div id="infowindow_content" ng-include src="\'info.html\'"></div><div>';
+                google.maps.event.addListener(marker, 'click', 
+                    (function( marker , scope, content , place ){
+                        return function(){
+                            $rootScope.place = place;
+                            var compiled = $compile(content)(scope);
+                            scope.$apply();
+                            scope.infoWindow.setContent( compiled[0] );
+                            scope.infoWindow.open( map , marker );                                        
+                        };
+                    })( marker , $scope, content , results[i] )
+                );
+            }
+            map.fitBounds(bounds);
+            $scope.$apply();
+        }
+    });
+    
+}
